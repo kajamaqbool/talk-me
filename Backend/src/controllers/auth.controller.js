bc8df8f3cd4161b9297b78419bfa5aa045cc1f31
kroos180805 <thoufiqh56@gmail.com>
2025-09-02 21:58:56 +0530

    Sign-in / Sign-up

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
@@ -20,12 +21,24 @@ export const signup = (req, res) => {
             password: hashedPassword
         });
 
-        if(!newUser){
+        if(newUser){
 
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePics: newUser.profilePics
+            });
         } else {
             res.status(400).json({message: "Invalid user data"});
         }
     } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal Server error"});
+    }
 };
 
 export const login = (req, res) => {
